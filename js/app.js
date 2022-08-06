9196bf0dca6ed159eed3fa4be4f084b94a3c5188
bigamigo00 <107258001+bigamigo00@users.noreply.github.com>
2022-08-07 00:44:31 +0500

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,12 @@
+let number1 = 100;
+let number2 = 200;
+
+let plus = number1 + number2;
+let multiplication = number1 * number2;
+let division = number2 / number1;
+let minus = number2 - number1;
+
+console.log(" 100+200" + "=" + plus);
+console.log("100*200" + "=" + multiplication);
+console.log("200/100" + "=" + division);
+console.log("200-100" + "=" + minus);
